f221919bdae8411689e3cc5b447b3e13d4f7fbb7
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-20 23:22:23 -0300

    Módulo 3, Exercício 1: Verificando imutabilidade da classe String.

---
+++ java3lang/TestaString.java
@@ -0,0 +1,19 @@
+package java3lang;
+
+public class TestaString {
+
+	 public static void main(String[] args) {
+         String s = " fj11 ";
+         s.replaceAll("1", "2");
+         System.out.println(s);
+         
+         s = s.replaceAll("1", "2");
+         System.out.println(s);
+         
+         System.out.println("Sem espaços " + s.trim());
+         System.out.println(s + " contém fj? " + s.contains("fj"));
+         System.out.println(s + " contém gj? " + s.contains("gj"));
+         System.out.println("Está vazia? " + s.isEmpty());
+         System.out.println("Quantos caracteres? " + s.length());
+     }
+}
