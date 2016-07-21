d5072365fe98357938bf6d9dab11636edce8db3b
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-20 23:27:02 -0300

    Módulo 3, Exercício 3: Comparando Integers.

---
+++ java3lang/TestaInteger.java
@@ -0,0 +1,22 @@
+package java3lang;
+
+public class TestaInteger {
+
+	public static void main(String[] args) {
+
+		Integer x1 = new Integer(10);
+		Integer x2 = new Integer(10);
+
+		if (x1 == x2) {
+			System.out.println("São iguais");
+		} else {
+			System.out.println("São diferentes");//Cai aqui, pois são referencias objetos diferentes.
+		}
+		
+		if (x1.equals(x2)) {
+			System.out.println("São iguais");//Cai aqui.
+		} else {
+			System.out.println("São diferentes");
+		}
+	}
+}
